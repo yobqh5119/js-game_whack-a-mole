f671cec4fc26612c4bc230f20b5a88c664df21c8
yoobin <yoobin.hong@ingka.ikea.com>
2021-07-03 01:24:47 -0400

    finished building out whack a mole game

---
+++ app.js
@@ -0,0 +1,47 @@
+const square = document.querySelectorAll('.square');
+const mole = document.querySelectorAll('.mole');
+const timeLeft = document.querySelector('#time-left');
+let scroe = document.querySelector('#score');
+
+let result = 0;
+let currentTime = timeLeft.textContent;
+
+function randomSquare() {
+  square.forEach(className => {
+    className.classList.remove('mole');
+  })
+  let randomPosition = square[Math.floor(Math.random() * 9)]
+  randomPosition.classList.add('mole');
+
+  // Assign the id of the randomPosition to hitPosition for us to use later
+  hitPosition = randomPosition.id;
+
+}
+
+square.forEach(id => {
+  id.addEventListener('mouseup', () => {
+    if(id.id === hitPosition) {
+      result = result + 1;
+      scroe.textContent = result;
+    }
+  })
+})
+
+function moveMole() {
+  let timerId = null;
+  timerId = setInterval(randomSquare, 1000);
+}
+
+moveMole()
+
+function countDown() {
+  currentTime--
+  timeLeft.textContent = currentTime
+
+  if (currentTime === 0) {
+    clearInterval(timerId);
+    alert('Game Over!')
+  }
+}
+
+let timerId = setInterval(countDown, 1000)
